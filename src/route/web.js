27fa7e5474f58e61a2bbb092b5789df7c0653e93
haruto3012 <leduyphuong3012@gmail.com>
2023-03-10 09:48:02 +0700

    haruto

---
+++ src/route/web.js
@@ -0,0 +1,24 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    //homeController
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
+    router.get('/crud', homeController.getCRUD);
+    router.post('/post-crud', homeController.postCRUD);
+    router.get('/get-crud', homeController.displayCRUD);
+    router.get('/edit-crud', homeController.getEditCRUD);
+    router.post('/put-crud', homeController.putCRUD);
+    router.get('/delete-crud', homeController.deleteCRUD);
+    //userController api
+    router.post('/api/login', userController.handleLogin);
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
+
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
---
+++ src/services/CRUDService.js
@@ -0,0 +1,117 @@
+import bcrypt from 'bcryptjs'
+import db from '../models/index';
+
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstname,
+                lastName: data.lastname,
+                address: data.address,
+                phoneNumber: data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                image: data.image,
+                roleId: data.roleId,
+            })
+
+            resolve('Create new user successful!')
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt);
+            resolve(hashPassword)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let getAllUser = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = db.User.findAll()
+            resolve(users)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let getUserInforById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId }
+            })
+
+            if (user) {
+                resolve(user)
+            } else {
+                resolve([])
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: data.id }
+            })
+
+            if (user) {
+                user.firstName = data.firstname
+                user.lastName = data.lastname
+                user.address = data.address
+                await user.save()
+                let allUsers = await db.User.findAll()
+                resolve(allUsers)
+            } else {
+                resolve()
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let deleteUserById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: {
+                    id: userId
+                }
+            })
+            if (user) {
+                await user.destroy()
+            }
+            resolve()
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+
+module.exports = {
+    createNewUser: createNewUser,
+    getAllUser: getAllUser,
+    getUserInforById: getUserInforById,
+    updateUserData: updateUserData,
+    deleteUserById: deleteUserById,
+}
